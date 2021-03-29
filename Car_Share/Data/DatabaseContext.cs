e0176e7f98a846f95508ff2c79e4b63b07e3e6d3
Mitchell Dennis <s3541094@student.rmit.edu.au>
2021-03-29 12:24:34 +1100

    +initial commit

---
+++ Car_Share/Data/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using CarShare.Models;
+namespace CarShare.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { }
+
+        // Standard Model Tables -- Uncomment once model is implemented
+        //public DbSet<Customer> Customer { get; set; }
+        //public DbSet<Account> Account { get; set; }
+        //public DbSet<Car> Car { get; set; }
+    }
+}
