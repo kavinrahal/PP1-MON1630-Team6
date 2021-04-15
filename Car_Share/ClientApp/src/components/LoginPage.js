c2ce989ed2807853e82cb05e4df225be8523ec13
Kavin Abeysinghe <s3756717@student.rmit.edu.au>
2021-04-15 18:49:58 +0530

    LoginPage frontend complete

---
+++ Car_Share/ClientApp/src/components/LoginPage.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './LoginPage.css';
+import Image from '../addons/loginlogo.png';
+import bg from '../addons/Circlebg.png';
+
+function LoginPage(){
+
+    return(
+        <div className = "LoginWrapper">
+            <div className = "row">
+                <div className = "longColumn">
+                    <div className = "row1 title">
+                        <div className = "whiteT">Car</div>
+                        <div className = "yellowT">Share</div>
+                        <div className = "whiteT">Scheme</div>
+                    </div>
+                    <div className = "quote">
+                        <div className = "drive">Drive Anywhere You Want,</div>
+                        <div className = "with">With Just a Few Clicks.</div>
+                        <img className = "midlogo" src = {Image}></img>
+                    </div>
+                    <div className = "about">Get to know more about us!</div>
+                </div>
+                <div className = "column">
+                    <div className = "loginSignup">
+                    
+                    
+                        <div className = "loginTitle">
+                            <div className = "Log">Log In</div>
+                        </div>
+                        <div className = "email">
+                            <div className = "emailLabel">Email</div>
+                            <input type = "text" className = "loginInput" placeholder = "  Email"></input>
+                        </div>
+                        <br></br>
+                        <div className = "password">
+                            <div className = "passwordLabel">Password</div>
+                            <input type = "password" className = "loginInput" placeholder = "  Password"></input>
+                        </div>
+                        <br></br>
+                        <button className = "loginBtn">Log In</button>
+                        <div className = "or">or</div>
+                        <div className = "join">
+                            <div className = "Join">Join Us Today!</div>
+                            <div className = "joinText">Don't Have an Account Yet?</div>
+                        </div>
+                        <br></br>
+                        <button className = "joinBtn">Sign Me Up!</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default LoginPage;
