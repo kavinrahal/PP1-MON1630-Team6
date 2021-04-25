7569e83b9b663e74eb2b69b8a36e4a29ab8ef798
Macintosh <macintosh@macintosh.local>
2021-04-25 15:36:23 +0800

    initial commit for past_booking TODO: Currently using dummy data, once backend is working, modify to request data using API Call TODO: once data is from backend, modify sort to use date. currently using ID. On hold as unsure of date formart.

--- Car_Share/ClientApp/src/App.js
+++ Car_Share/ClientApp/src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
@@ -9,22 +9,24 @@ import React, { Component } from 'react';
 import './custom.css'
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import BookingHistory from "./components/BookingHistory";
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Router>
         <div className="App">
           <Switch>
-            <Route path = "/" exact component = {LoginPage}></Route>
-            <Route path = "/register" exact component = {RegisterPage}></Route>
-          
-          
+            <Route path="/" exact component={LoginPage}></Route>
+            <Route path="/register" exact component={RegisterPage}></Route>
+            <Route path="/booking_history" exact component={BookingHistory}></Route>
+
+
           </Switch>
         </div>
-    </Router>
+      </Router>
     );
   }
 }
