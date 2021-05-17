2e7ca33dd4516dba9262a4c4ebe03a12bbb419ec
Macintosh <macintosh@macintosh.local>
2021-05-17 11:12:07 +0800

    Inital commit of register_login
    awaiting backend apis to be up to continue further testing
    
    confirmation of backend data input/retrieval
    
    further add sessionStorage once able to validate login

--- Car_Share/ClientApp/src/components/LoginPage.js
+++ Car_Share/ClientApp/src/components/LoginPage.js
@@ -1,61 +1,88 @@
-import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-import './styles/LoginPage.css'
-import Image from '../addons/loginlogo.png';
-import bg from '../addons/circlebg.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./styles/LoginPage.css";
+import Image from "../addons/loginlogo.png";
+import bg from "../addons/circlebg.svg";
 
-function LoginPage(){
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
-        <div className = "LoginWrapper">
-            <div className = "row">
-                <div className = "longColumn">
-                    <div className = "row1 title">
-                        <div className = "whiteT">Car</div>
-                        <div className = "yellowT">Share</div>
-                        <div className = "whiteT">Scheme</div>
-                    </div>
-                    <div className = "quote">
-                        <div className = "drive">Drive Anywhere You Want,</div>
-                        <div className = "with">With Just a Few Clicks.</div>
-                        <img className = "midlogo" src = {Image}></img>
-                    </div>
-                    <div className = "about">Get to know more about us!</div>
-                </div>
-                <div className = "column">
-                    <div className = "loginSignup">
-                        <div className = "loginTitle">
-                            <div className = "Log">Log In</div>
-                        </div>
-                        <div className = "email">
-                            <div className = "emailLabel">Email</div>
-                            <input type = "text" className = "loginInput" placeholder = "  Email"></input>
-                        </div>
-                        <br></br>
-                        <div className = "password">
-                            <div className = "passwordLabel">Password</div>
-                            <input type = "password" className = "loginInput" placeholder = "  Password"></input>
-                        </div>
-                        <br></br>
-                        <Link to = "/dashboard"><button className = "loginBtn">Log In</button></Link>
-                        <div className = "or">or</div>
-                        <div className = "join">
-                            <div className = "Join">Join Us Today!</div>
-                            <div className = "joinText">Don't Have an Account Yet?</div>
-                        </div>
-                        <br></br>
-                        <Link to = "/register"><button className = "joinBtn">Sign Me Up!</button></Link>
-                    </div>
-                </div>
-            </div>
+  const handleSubmit = async () => {
+    executeBasicAuthenticationService(email, password).then((response) => {
+      console.log(response.data.name);
+    });
+  };
+  const executeBasicAuthenticationService = async (email, password) => {
+    // Authenicates User and passes username and password to backend
+    const res = await fetch(`http://localhost:5001/auth/${email}/${password}`, {
+      headers: {
+        authorization: createBasicAuthToken(email, password),
+      },
+    });
+    const data = await res.json();
 
+    return data;
+  };
+  const createBasicAuthToken = (email, password) => {
+    return "Basic " + window.btoa(username + ":" + password);
+  };
+  return (
+    <div className="LoginWrapper">
+      <div className="row">
+        <div className="longColumn">
+          <div className="row1 title">
+            <div className="whiteT">Car</div>
+            <div className="yellowT">Share</div>
+            <div className="whiteT">Scheme</div>
+          </div>
+          <div className="quote">
+            <div className="drive">Drive Anywhere You Want,</div>
+            <div className="with">With Just a Few Clicks.</div>
+            <img className="midlogo" src={Image}></img>
+          </div>
+          <div className="about">Get to know more about us!</div>
         </div>
-    );
+        <div className="column">
+          <div className="loginSignup">
+            <div className="loginTitle">
+              <div className="Log">Log In</div>
+            </div>
+            <div className="email">
+              <div className="emailLabel">Email</div>
+              <input
+                type="text"
+                className="loginInput"
+                placeholder="  Email"
+              ></input>
+            </div>
+            <br></br>
+            <div className="password">
+              <div className="passwordLabel">Password</div>
+              <input
+                type="password"
+                className="loginInput"
+                placeholder="  Password"
+              ></input>
+            </div>
+            <br></br>
+            <Link to="/dashboard">
+              <button className="loginBtn">Log In</button>
+            </Link>
+            <div className="or">or</div>
+            <div className="join">
+              <div className="Join">Join Us Today!</div>
+              <div className="joinText">Don't Have an Account Yet?</div>
+            </div>
+            <br></br>
+            <Link to="/register">
+              <button className="joinBtn">Sign Me Up!</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
--- Car_Share/ClientApp/src/components/RegisterPage.js
+++ Car_Share/ClientApp/src/components/RegisterPage.js
@@ -1,91 +1,193 @@
-import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-import './styles/RegisterPage.css';
-import Image from '../addons/reglogo.png';
-import BackDrop from '../addons/City Backdrop.svg';
+import React, { useState, Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useHistory } from "react-router";
+import "./styles/RegisterPage.css";
+import Image from "../addons/reglogo.png";
+import BackDrop from "../addons/City Backdrop.svg";
 
-function RegisterPage(){
-    return(
-        <div className = "registerWrapper">
-            <div className = "signUpRow">
-                <div className = "signUpColumn">
-                    <div className = "row1 title">
-                        <div className = "whiteT">Car</div>
-                        <div className = "yellowT">Share</div>
-                        <div className = "whiteT">Scheme</div>
-                    </div>
-                    <div className = "quote">
-                        <div className = "need">Need a ride but just for yourself?</div>
-                        <div className = "we">We've got you covered.</div>
-                        <img className = "signUpMidlogo" src = {Image}></img>
-                    </div>
-                    <div className = "low">
-                        <img className = "lowlogo" src = {BackDrop}></img>
-                    </div>
-                </div>
-                <div className = "signUplongColumn">
-                    <div className = "signUpForm">
-                        <div className = "signUpTitle">
-                            <div className = 'sign'>Sign Up</div>
-                        </div>
-                        <br></br>
-                        <div className = "description">
-                            <div className = "desc">Let’ s get you all set up so you can verify your 
-                            personal account and begin setting up your profile.</div>
-                        </div>
-                        <br></br>
-                        <div className = "fields">
-                            <div className = "names">
-                                <div className = "first">
-                                    <div className = "signUpLabel">Name</div>
-                                    <input type = "text" className = "signUpText" placeholder = "  First Name"></input>
-                                </div>
-                                <div className = "last">
-                                    <div className = "signUpLabel">Address</div>
-                                    <input type = "text" className = "signUpText" placeholder = "  Last Name"></input>
-                                </div>
-                            </div>
-                            <br></br>
-                            <div className = "contact">
-                                <div className = "signUpEmail">
-                                    <div className = "signUpLabel">Email</div>
-                                    <input type = "email" className = "signUpText" placeholder = "  Email"></input>
-                                </div>
-                                <div className = "phone">
-                                    <div className = "signUpLabel">Phone Number</div>
-                                    <input type = "text" className = "signUpText" placeholder = "  Phone Number"></input>
-                                </div>
-                            </div>
-                            <br></br>
-                            <div className = "passwords">
-                                <div className = "signUpPassword">
-                                    <div className = "signUpLabel">Password</div>
-                                    <input type = "password" className = "signUpText" placeholder = "  Password"></input>
-                                </div>
-                                <div className = "confirmPassword">
-                                    <div className = "signUpLabel">Confirm Password</div>
-                                    <input type = "password" className = "signUpText" placeholder = "  Confirm Password"></input>
-                                </div>
-                            </div>
-                            <br></br>
-                            <button className = "confirmSignUpBtn">Sign Up!</button>
-                            <br></br>
-                            <br></br>
-                            <div className = "already">
-                                <div className = "alreadyText">Already Have an Account?</div>
-                                <div className = "alreadyLog"><Link to = "/">Log In!</Link></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [hasRegisterFailed, setHasRegisteredFailed] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+    if (password != confirmPassword) {
+      alert("Password Mismatch!");
+      history.push({
+        pathname: "/",
+        state: {
+          response: "messageFromServer",
+        },
+      });
+      return;
+    }
+
+    const customer = {
+      name: name,
+      email: email,
+      address: address,
+      mobile: mobile,
+      password: password,
+    };
+    console.log(customer);
+
+    const res = await fetch("https://localhost:5001/api/Customer/1", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: customer,
+    })
+      .then((response) => {
+        const d = res.json();
+        console.log(d);
+        if (response.data != null) {
+          // Reset data
+          setName("");
+          setEmail("");
+          setAddress("");
+          setMobile("");
+          setPassword("");
+          setConfirmPassword("");
+
+          console.log(response);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        // setHasRegisteredFailed(true);
+      });
+  };
+
+  return (
+    <div className="registerWrapper">
+      <div className="signUpRow">
+        <div className="signUpColumn">
+          <div className="row1 title">
+            <div className="whiteT">Car</div>
+            <div className="yellowT">Share</div>
+            <div className="whiteT">Scheme</div>
+          </div>
+          <div className="quote">
+            <div className="need">Need a ride but just for yourself?</div>
+            <div className="we">We've got you covered.</div>
+            <img className="signUpMidlogo" src={Image}></img>
+          </div>
+          <div className="low">
+            <img className="lowlogo" src={BackDrop}></img>
+          </div>
         </div>
-    );
+        <div className="signUplongColumn">
+          <div className="signUpForm">
+            <div className="signUpTitle">
+              <div className="sign">Sign Up</div>
+            </div>
+            <br></br>
+            <div className="description">
+              <div className="desc">
+                Let’ s get you all set up so you can verify your personal
+                account and begin setting up your profile.
+              </div>
+            </div>
+            <br></br>
+            <div className="fields">
+              <div className="names">
+                <div className="first">
+                  <div className="signUpLabel">Name</div>
+                  <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    className="signUpText"
+                    placeholder="  First Name"
+                  ></input>
+                </div>
+                <div className="last">
+                  <div className="signUpLabel">Address</div>
+                  <input
+                    onChange={(e) => setAddress(e.target.value)}
+                    type="text"
+                    className="signUpText"
+                    placeholder="  Address"
+                  ></input>
+                </div>
+              </div>
+              <br></br>
+              <div className="contact">
+                <div className="signUpEmail">
+                  <div className="signUpLabel">Email</div>
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    className="signUpText"
+                    placeholder="  Email"
+                  ></input>
+                </div>
+                <div className="phone">
+                  <div className="signUpLabel">Phone Number</div>
+                  <input
+                    onChange={(e) => setMobile(e.target.value)}
+                    type="text"
+                    className="signUpText"
+                    placeholder="  Phone Number"
+                  ></input>
+                </div>
+              </div>
+              <br></br>
+              <div className="passwords">
+                <div className="signUpPassword">
+                  <div className="signUpLabel">Password</div>
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    className="signUpText"
+                    placeholder="  Password"
+                  ></input>
+                </div>
+                <div className="confirmPassword">
+                  <div className="signUpLabel">Confirm Password</div>
+                  <input
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    type="password"
+                    className="signUpText"
+                    placeholder="  Confirm Password"
+                  ></input>
+                </div>
+                {hasRegisterFailed && (
+                  <div className="alert alert-danger">
+                    {" "}
+                    Invalid Credentials{" "}
+                  </div>
+                )}
+              </div>
+              <br></br>
+              <button
+                className="confirmSignUpBtn"
+                onClick={() => handleSubmit()}
+              >
+                Sign Up Post
+              </button>
+              <button className="confirmSignUpBtn">Sign Up!</button>
+              <br></br>
+              <br></br>
+              <div className="already">
+                <div className="alreadyText">Already Have an Account?</div>
+                <div className="alreadyLog">
+                  <Link to="/">Log In!</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RegisterPage;
