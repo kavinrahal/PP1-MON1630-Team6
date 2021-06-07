309a81abe98071363cc54dfbb6e9d9cc17187d2c
Kavin Abeysinghe <s3756717@student.rmit.edu.au>
2021-06-07 15:06:59 +0530

    Calculate distance + filter

--- Car_Share/ClientApp/src/components/geolocation.jsx
+++ Car_Share/ClientApp/src/components/geolocation.jsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState, useRef } from 'react';
-import { GoogleMap,  Marker, InfoWindow, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap,  Marker, InfoWindow, useJsApiLoader, useGoogleMap } from '@react-google-maps/api';
+import { DirectionsRenderer, DirectionsService} from '@react-google-maps/api';
 import carData from "./carData.json";
 
 export const MapContainer = () => {
 
+    const google = window.google = window.google ? window.google : {};
     const mapStyles = {
         width: '400px',
         height: '400px'
@@ -59,6 +61,36 @@ export const MapContainer = () => {
 
     return RenderMap()
 
+    function haversine_distance() {
+        var R = 6371; // Radius of the Earth in km
+        var rlat1 = currentPos.lat * (Math.PI/180); // Convert degrees to radians
+        var rlat2 = selected.location.lat * (Math.PI/180); // Convert degrees to radians
+        var difflat = rlat2-rlat1; // Radian difference (latitudes)
+        var difflon = (currentPos.lng-selected.location.lng) * (Math.PI/180); // Radian difference (longitudes)
+  
+        var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+        return d;
+    }
+
+    
+
+    function calcRoute() {
+        var directionsDisplay = new google.maps.DirectionsRenderer();
+        var directionsService = new google.maps.DirectionsService();
+        var start = new google.maps.LatLng(37.334818, -121.884886);
+        var end = new google.maps.LatLng(37.441883, -122.143019);
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
+    }            
+
     function RenderMap() {
         const { isLoaded } = useJsApiLoader({
             id: 'google-map-script',
@@ -92,14 +124,22 @@ export const MapContainer = () => {
                 }
 
                 { 
-                    carData.map(item => {
-                        return (
-                            <Marker key={item.name} position={item.location} onClick={() => onSelect(item)}/>
-                        )
+                     carData.map(item => {
+                        if (DDselected.type == item.type){
+                            return (
+                                <Marker key={item.name} position={item.location} onClick={() => onSelect(item)}/>
+                            )
+                        }
+
+                        if(DDselected.type == null){
+                            return (
+                                <Marker key={item.name} position={item.location} onClick={() => onSelect(item)}/>
+                            )
+                        }
                     })
                 }
                 
-                {
+                {   
                     selected.location && 
                     (
                     <InfoWindow
@@ -112,11 +152,14 @@ export const MapContainer = () => {
                     <p>{"Type: " + selected.type}</p>
                     <p>{"Make: " + selected.make}</p>
                     <p>{"Model: " + selected.model}</p>
+                    <button onClick = {calcRoute()}>Calc</button>
                     </>
                     </InfoWindow>
                     )
                 }
-            </GoogleMap>
+
+                
+                </GoogleMap>
             <div style={{width:200}}>
             <select value={DDselected.type} onChange={handleChange} onClick={filterByType}>
                     {
@@ -127,9 +170,25 @@ export const MapContainer = () => {
                     ))}
                 </select>
             </div>
+            {
+                selected.location && 
+                <div className = "selectedCar">
+                    <div className = "cardeets">
+                            <h2>Car Details</h2>
+                            <p>{selected.name}</p>
+                            <p>Distance: {haversine_distance().toFixed(2)} km</p>
+                            <p>{"Type: " + selected.type}</p>
+                            <p>{"Make: " + selected.make}</p>
+                            <p>{"Model: " + selected.model}</p>
+                    </div>
+                </div>
+            }
+            
         </div>  
         )  : <></>
     }
+
+    
 }
  
 export default MapContainer;
